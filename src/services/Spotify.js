723dfc6aba10c8818be622d3e2a35ac04fe6df19
Daniel Ståhl <oe.daniel.stahl@gmail.com>
2017-12-14 22:48:48 +0100

    Redirect via env

--- src/services/Spotify.js
+++ src/services/Spotify.js
@@ -6,8 +6,7 @@ class Spotify {
   static handlelogin() {
     var ACCOUNTS_BASE_URL = 'https://accounts.spotify.com'
     var CLIENT_ID = '1b24de0b94324459b855aa136d301949'
-    var REDIRECT_URI = 'http://localhost:3000'
-
+    var REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI
     var scopes = [
       'user-read-playback-state',
       'user-modify-playback-state',
